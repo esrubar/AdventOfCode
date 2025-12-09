fd564425031525c921bed0a534c9d7dce8fd92a7
Estela <estela@MacBook-Pro-de-Estela.local>
2025-12-09 13:11:11 +0100

    Day 4 - Part 2

--- AdventOfCode/2025/Day4/Day4.cs
+++ AdventOfCode/2025/Day4/Day4.cs
@@ -7,16 +7,22 @@ public static class Day4
         var input = File.ReadAllLines("Day4/Input.txt");
         var rows = input.Length;
         var cols = input[0].Length;
-
-        // Desplazamientos a las 8 direcciones
+        
         var dirs = new[,]
         {
             {-1, -1}, {-1, 0}, {-1, 1},
             { 0, -1},          { 0, 1},
             { 1, -1}, { 1, 0}, { 1, 1}
         };
+        
+        //var result = Part1(input, cols, rows, dirs);
+        var result = Part2(input, rows, cols, dirs);
+        Console.WriteLine("Result: " + result);
+    }
 
-        int accessible = 0;
+    private static int Part1(string[] input, int cols, int rows, int[,] dirs)
+    {
+        var accessible = 0;
 
         for (var i = 0; i < rows; i++)
         {
@@ -43,7 +49,62 @@ public static class Day4
             }
         }
 
-        Console.WriteLine("Result: " + accessible);
+        return accessible;
+    }
+    
+    private static int Part2(string[] input, int cols, int rows, int[,] dirs)
+    {
+        
+        var matrix = input.Select(line => line.ToCharArray()).ToArray();
+        
+        int accessible;
+        var result = 0;
+        var positionsToChange = new List<string>();
+        
+        do
+        {
+            accessible = 0;
+            positionsToChange.Clear();
+            for (var i = 0; i < rows; i++)
+            {
+                for (var j = 0; j < cols; j++)
+                {
+                    if (matrix[i][j] != '@')
+                        continue;
+
+                    var count = 0;
+
+                    for (var d = 0; d < 8; d++)
+                    {
+                        var ni = i + dirs[d, 0];
+                        var nj = j + dirs[d, 1];
+
+                        if (ni < 0 || ni >= rows || nj < 0 || nj >= cols) continue;
+                    
+                        if (matrix[ni][nj] == '@')
+                            count++;
+                    }
+
+                    if (count >= 4) continue;
+                    accessible++;
+                    positionsToChange.Add(i + "," + j);
+                }
+            }
+            result += accessible;
+            
+            foreach (var pos in positionsToChange)
+            {
+                var parts = pos.Split(',');
+                var i = int.Parse(parts[0]);
+                var j = int.Parse(parts[1]);
+
+                matrix[i][j] = '.';
+            }
+            
+        } while (accessible >= 1);
+
+
+        return result;
     }
 
 }
